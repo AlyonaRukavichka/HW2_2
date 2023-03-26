c8606ed2a44d3bc49faca6ee9d64097606cbb0db
Alyona Rukavishnikova <a.rukavichka14@gmail.com>
2023-03-27 01:04:31 +0300

    task 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int firstAmount = 400;
+        int topUp = 1100;
+
+        int bonus;
+        if (firstAmount + topUp > 1000) {
+            bonus = (firstAmount + topUp - 1000) / 100;
+        }
+        else {
+            bonus = 0;
+        }
+        System.out.println(bonus);
+        int sum = firstAmount + topUp + bonus;
+        System.out.println(sum);
     }
 }
